b0bbe3eb927a28b9ffdf0ee0e123a2492eb269d5
Dennis Muturi <deynohmuturi@gmail.com>
2025-01-27 18:55:37 +0300

    Implement search functions for recipes and foods in MeiliSearch integration

--- src/Search/meiliFoodSearch.ts
+++ src/Search/meiliFoodSearch.ts
@@ -1,9 +1,6 @@
 import { MeiliSearch } from 'meilisearch'
-import { readFileSync } from 'fs'
-const foods = JSON.parse(readFileSync('./json_foods.json','utf8'))
-const recipes = JSON.parse(readFileSync('./json_recipes.json','utf8'))
-const kenyanRecipes = JSON.parse(readFileSync('./json_kenyan_recipes.json','utf8'))
 import { config } from 'dotenv'
+import { KenyanRecipe, parseKenyanRecipes, parseRecipes, Recipe } from '../sqlDB/mysqlDB.js'
 config()
 const host = process.env.MEILI_HOST || 'default_host';
 const apiKey = process.env.MEILI_API_KEY || 'default_api_key';
@@ -18,7 +15,36 @@ const client = new MeiliSearch({
 // client.index('recipes').addDocuments(recipes,{primaryKey:'uuid'})
 //     .then((res) => console.log(res))
 
-client.index('recipes').search('cheese',{attributesToSearchOn:['NER','recipe_name'],limit:10}).then((res) => console.log(res))
+export async function searchRecipes(searchTerm:string,region:string){
+  if(region=='kenyan'){
+    const results=await client.index('kenyanRecipes').search(searchTerm,{attributesToSearchOn:['ingridients','recipe_name'],limit:10})
+    console.log(results)
+    return parseKenyanRecipes(results.hits as KenyanRecipe[])  
+    // return results.hits
+  }
+  else{
+    const results=await client.index('recipes').search(searchTerm,{attributesToSearchOn:['NER','recipe_name'],limit:10})
+    console.log(results)
+    return parseRecipes(results.hits as Recipe[])
+  } 
+}
+export async function searchFoods(searchTerm:string){
+  const results=await client.index('foods').search(searchTerm,{attributesToSearchOn:['food_name'],limit:10})
+  console.log(results)
+  return results.hits
+}
+// client.index('recipes').updateSearchableAttributes([
+//   'recipe_name',
+//   'NER',
+// ]
+// )
+// client.index('kenyanRecipes').updateSearchableAttributes([
+//   'recipe_name',
+//   'ingridients',
+// ]
+// )
+
+// client.index('recipes').search('cheese',{attributesToSearchOn:['NER','recipe_name'],limit:10}).then((res) => console.log(res))
 
   
 
